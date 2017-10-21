3f92b6cc8d6698785246088a1f38b6ea71aa527a
jianfengzhang <zhangjianfeng@kylinos.cn>
2017-10-21 20:40:24 +0800

    chapter 3: copy_block by block

--- chp3/copy_system.c
+++ chp3/copy_system.c
@@ -5,13 +5,14 @@
 
 int main()
 {
-	char c;
+	char block[10];
 	int in,out;
-	
+	int nread;
+
 	in=open("file.in",O_RDONLY);
 	out=open("file.out",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR);
-	while(read(in,&c,1)==1)
-		write(out,&c,1);
+	while((nread=read(in,block,sizeof(block)))>0)
+		write(out,block,nread);
 
 	exit(0);
 }
